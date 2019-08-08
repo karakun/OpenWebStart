42a39fb64f508444cf9e00c8c944a37df627e4ba
Stephan Classen <st.classen@gmx.ch>
2019-08-08 16:02:21 +0200

    search for openwebstart.jar when launching new JVM instance

--- openwebstart/src/main/java/com/openwebstart/launcher/OwsJvmLauncher.java
+++ openwebstart/src/main/java/com/openwebstart/launcher/OwsJvmLauncher.java
@@ -1,16 +1,19 @@
 package com.openwebstart.launcher;
 
-import java.io.File;
-import java.util.LinkedList;
-import java.util.List;
 import net.adoptopenjdk.icedteaweb.JvmPropertyConstants;
 import net.adoptopenjdk.icedteaweb.StreamUtils;
-import net.adoptopenjdk.icedteaweb.jvm.JvmUtils;
 import net.adoptopenjdk.icedteaweb.launch.JvmLauncher;
 import net.adoptopenjdk.icedteaweb.logging.Logger;
 import net.adoptopenjdk.icedteaweb.logging.LoggerFactory;
 import net.sourceforge.jnlp.JNLPFile;
 import net.sourceforge.jnlp.runtime.Boot;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static com.openwebstart.util.PathQuoteUtil.quoteIfRequired;
 
@@ -24,22 +27,22 @@ class OwsJvmLauncher implements JvmLauncher {
     public void launchExternal(JNLPFile jnlpFile, List<String> args) throws Exception {
         final String javaBinary = "/bin/java".replace('/', File.separatorChar);
         final String pathToJavaBinary = System.getProperty(JvmPropertyConstants.JAVA_HOME) + javaBinary;
-        final String pathToJavaws = JvmUtils.getPathToJavawsJar();
-        launchExternal(pathToJavaBinary, pathToJavaws, jnlpFile.getNewVMArgs(), args);
+        final String pathToJar = getPathToOpenWebStartJar();
+        launchExternal(pathToJavaBinary, pathToJar, jnlpFile.getNewVMArgs(), args);
     }
 
     /**
      * @param pathToJavaBinary path to the java binary of the JRE in which to start OWS
-     * @param pathToJavawsJar  path to the javaws.jar included in OWS
+     * @param pathToJar        path to the openwebstart.jar included in OWS
      * @param vmArgs           the arguments to pass to the jvm
      * @param javawsArgs       the arguments to pass to javaws (aka IcedTea-Web)
      */
-    private void launchExternal(String pathToJavaBinary, String pathToJavawsJar, List<String> vmArgs, List<String> javawsArgs) throws Exception {
+    private void launchExternal(String pathToJavaBinary, String pathToJar, List<String> vmArgs, List<String> javawsArgs) throws Exception {
         final List<String> commands = new LinkedList<>();
 
         commands.add(quoteIfRequired(pathToJavaBinary));
         commands.add("-cp");
-        commands.add(quoteIfRequired(pathToJavawsJar));
+        commands.add(quoteIfRequired(pathToJar));
 
         commands.addAll(vmArgs);
         commands.add(Boot.class.getName());
@@ -53,6 +56,26 @@ class OwsJvmLauncher implements JvmLauncher {
                 .start();
 
         StreamUtils.waitForSafely(p);
+    }
+
+    private static String getPathToOpenWebStartJar() {
+        final String classPath = System.getProperty("java.class.path");
+        final String pathSeparator = System.getProperty("path.separator");
+        final String javaHome = System.getProperty("java.home");
+        final String[] classpathElements = classPath.split(Pattern.quote(pathSeparator));
+
+        final List<String> jarCandidates = Arrays.stream(classpathElements)
+                .filter((e) -> e.endsWith("openwebstart.jar"))
+                .filter((e) -> !e.startsWith(javaHome))
+                .collect(Collectors.toList());
+
+        if (jarCandidates.size() == 1) {
+            return jarCandidates.get(0);
+        } else if (jarCandidates.size() > 1) {
+            throw new IllegalStateException("multiple openwebstart jars found in classpath");
+        } else {
+            throw new IllegalStateException("openwebstart jar not found in classpath");
+        }
     }
 
 }
