7afa8443523f0e0b9c810728a65b00010c592a26
Stephan Classen <st.classen@gmx.ch>
2019-08-29 16:09:49 +0200

    improve properties name and constant

--- openwebstart/src/main/java/com/openwebstart/config/OwsDefaultsProvider.java
+++ openwebstart/src/main/java/com/openwebstart/config/OwsDefaultsProvider.java
@@ -14,10 +14,10 @@ import java.util.stream.Stream;
 public class OwsDefaultsProvider implements DefaultsProvider {
 
     public static final String DEFAULT_JVM_DOWNLOAD_SERVER = "ows.jvm.manager.server.default";
-    public static final String ALLOWS_NON_DEFAULT_JVM_DOWNLOAD_SERVER = "ows.jvm.manager.server.allowOthers";
-    public static final String DEFAULT_JVM_VENDOR = "ows.jvm.manager.vendor.default";
-    public static final String JVM_UPDATE_STRATEGY = "ows.jvm.manager.update.strategy";
-    public static final String JVM_SUPPORTED_VERSION_RANGE = "ows.jvm.manager.version.range";
+    public static final String ALLOW_DOWNLOAD_SERVER_FROM_JNLP = "ows.jvm.manager.server.allowFromJnlp";
+    public static final String JVM_VENDOR = "ows.jvm.manager.vendor";
+    public static final String JVM_UPDATE_STRATEGY = "ows.jvm.manager.updateStrategy";
+    public static final String JVM_SUPPORTED_VERSION_RANGE = "ows.jvm.manager.versionRange";
 
     public static final RuntimeUpdateStrategy DEFAULT_UPDATE_STRATEGY = RuntimeUpdateStrategy.ASK_FOR_UPDATE_ON_LOCAL_MATCH;
 
@@ -35,12 +35,12 @@ public class OwsDefaultsProvider implements DefaultsProvider {
                         ValidatorFactory.createUrlValidator()
                 ),
                 Setting.createDefault(
-                        ALLOWS_NON_DEFAULT_JVM_DOWNLOAD_SERVER,
+                        ALLOW_DOWNLOAD_SERVER_FROM_JNLP,
                         Boolean.FALSE.toString(),
                         ValidatorFactory.createBooleanValidator()
                 ),
                 Setting.createDefault(
-                        DEFAULT_JVM_VENDOR,
+                        JVM_VENDOR,
                         "AdoptOpenJDK",
                         null
                 ),
