df352fa9b3afeac865517504ebc176cb8f4e906d
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-10-04 14:51:07 +0200

    New Layout for JVM Config Dialog

--- openwebstart/src/main/java/com/openwebstart/jvm/ui/dialogs/ConfigurationDialog.java
+++ openwebstart/src/main/java/com/openwebstart/jvm/ui/dialogs/ConfigurationDialog.java
@@ -1,5 +1,7 @@
 package com.openwebstart.jvm.ui.dialogs;
 
+import com.openwebstart.controlpanel.ButtonPanelFactory;
+import com.openwebstart.controlpanel.FormPanel;
 import com.openwebstart.jvm.JavaRuntimeManager;
 import com.openwebstart.jvm.RuntimeManagerConfig;
 import com.openwebstart.jvm.RuntimeUpdateStrategy;
@@ -10,12 +12,16 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.BorderLayout;
 import java.awt.Checkbox;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -42,8 +48,8 @@ public class ConfigurationDialog extends ModalDialog {
         final JTextField defaultUpdateServerField = new JTextField();
         defaultUpdateServerField.setText(Optional.ofNullable(RuntimeManagerConfig.getDefaultRemoteEndpoint()).map(URL::toString).orElse(""));
 
-        final Checkbox allowAnyUpdateServerCheckBox = new Checkbox("Allow server from JNLP file");
-        allowAnyUpdateServerCheckBox.setState(RuntimeManagerConfig.isNonDefaultServerAllowed());
+        final JCheckBox allowAnyUpdateServerCheckBox = new JCheckBox("Allow server from JNLP file");
+        allowAnyUpdateServerCheckBox.setSelected(RuntimeManagerConfig.isNonDefaultServerAllowed());
 
         final JLabel supportedVersionRangeLabel = new JLabel("Restrict JVM version range:");
         final JTextField supportedVersionRangeField = new JTextField();
@@ -55,7 +61,7 @@ public class ConfigurationDialog extends ModalDialog {
                 RuntimeManagerConfig.setStrategy((RuntimeUpdateStrategy) updateStrategyComboBox.getSelectedItem());
                 RuntimeManagerConfig.setDefaultVendor((String) vendorComboBox.getSelectedItem());
                 RuntimeManagerConfig.setDefaultRemoteEndpoint(new URI(defaultUpdateServerField.getText()));
-                RuntimeManagerConfig.setNonDefaultServerAllowed(allowAnyUpdateServerCheckBox.getState());
+                RuntimeManagerConfig.setNonDefaultServerAllowed(allowAnyUpdateServerCheckBox.isSelected());
                 RuntimeManagerConfig.setSupportedVersionRange(Optional.ofNullable(supportedVersionRangeField.getText()).filter(t -> !t.trim().isEmpty()).map(VersionString::fromString).orElse(null));
                 close();
             } catch (URISyntaxException ex) {
@@ -67,31 +73,43 @@ public class ConfigurationDialog extends ModalDialog {
         cancelButton.addActionListener(e -> close());
 
 
-        final JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(0, 2, 6, 2));
-        mainPanel.add(updateStrategyLabel);
-        mainPanel.add(updateStrategyComboBox);
-        mainPanel.add(defaultVendorLabel);
-        mainPanel.add(vendorComboBox);
-        mainPanel.add(defaultUpdateServerLabel);
-        mainPanel.add(defaultUpdateServerField);
-        mainPanel.add(new JPanel());
-        mainPanel.add(allowAnyUpdateServerCheckBox);
-        mainPanel.add(supportedVersionRangeLabel);
-        mainPanel.add(supportedVersionRangeField);
+        final FormPanel mainPanel = new FormPanel();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
+
+
+        final GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx=0;
+        constraints.gridy=0;
+
+        mainPanel.addRow(0, updateStrategyLabel, updateStrategyComboBox);
+        mainPanel.addRow(1, defaultVendorLabel, vendorComboBox);
+        mainPanel.addRow(2, defaultUpdateServerLabel, defaultUpdateServerField);
+        mainPanel.addEditorRow(3, allowAnyUpdateServerCheckBox);
+        mainPanel.addRow(4, supportedVersionRangeLabel, supportedVersionRangeField);
+        mainPanel.addFlexibleRow(5);
 
         final JPanel actionWrapperPanel = new JPanel();
+        actionWrapperPanel.setBackground(Color.WHITE);
         actionWrapperPanel.setLayout(new BoxLayout(actionWrapperPanel, BoxLayout.LINE_AXIS));
-        actionWrapperPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+        actionWrapperPanel.setBorder(BorderFactory.createEmptyBorder(4, 8, 4, 8));
         actionWrapperPanel.add(Box.createHorizontalGlue());
         actionWrapperPanel.add(okButton);
         actionWrapperPanel.add(cancelButton);
 
+        final JPanel topBorder = new JPanel();
+        topBorder.setBackground(Color.GRAY);
+        topBorder.setPreferredSize(new Dimension(1, 1));
+        topBorder.setMaximumSize(new Dimension(Integer.MAX_VALUE, 1));
+        topBorder.setMinimumSize(new Dimension(1, 1));
+
+        final JPanel actionPanel = new JPanel(new BorderLayout());
+        actionPanel.add(topBorder, BorderLayout.NORTH);
+        actionPanel.add(actionWrapperPanel, BorderLayout.CENTER);
+
         final JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout(8, 8));
-        panel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
         panel.add(mainPanel, BorderLayout.CENTER);
-        panel.add(actionWrapperPanel, BorderLayout.SOUTH);
+        panel.add(ButtonPanelFactory.createButtonPanel(okButton, cancelButton), BorderLayout.SOUTH);
 
         add(panel);
     }
