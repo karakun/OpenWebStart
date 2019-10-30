7e50727e000b5b148f8749ed86f4928056011506
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-10-30 16:41:21 +0100

    Catching DLL Error

--- openwebstart/src/main/java/com/openwebstart/launcher/ControlPanelLauncher.java
+++ openwebstart/src/main/java/com/openwebstart/launcher/ControlPanelLauncher.java
@@ -32,9 +32,12 @@ public class ControlPanelLauncher {
 
         try {
             new InitialConfigurationCheck(config).check();
-        } catch (Exception e) {
+        } catch (final Exception e) {
             DialogFactory.showErrorDialog(Translator.getInstance().translate("error.initialConfig"), e);
             System.exit(-1);
+        } catch (final UnsatisfiedLinkError e) {
+            //TODO: this exception is thrown on windows if you start OWS from the ide instead of using install4J
+            LOG.error("Initial configuration was not checked. This normally happens on Windows systems if you start OWS from the IDE.", e);
         }
 
         try {
