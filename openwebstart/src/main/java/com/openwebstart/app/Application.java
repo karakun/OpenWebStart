778fde85ee0544bfd6a06549cec922172a8f8e5a
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-11-15 14:16:08 +0100

    warnings removed

--- openwebstart/src/main/java/com/openwebstart/app/Application.java
+++ openwebstart/src/main/java/com/openwebstart/app/Application.java
@@ -3,6 +3,7 @@ package com.openwebstart.app;
 import com.openwebstart.os.linux.FavIcon;
 import net.adoptopenjdk.icedteaweb.Assert;
 import net.adoptopenjdk.icedteaweb.jnlp.element.information.IconKind;
+import net.adoptopenjdk.icedteaweb.resources.cache.CacheFile;
 import net.adoptopenjdk.icedteaweb.resources.cache.CacheId;
 import net.adoptopenjdk.icedteaweb.xmlparser.ParseException;
 import net.sourceforge.jnlp.JNLPFile;
@@ -10,7 +11,6 @@ import net.sourceforge.jnlp.JNLPFile;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -37,7 +37,7 @@ public class Application {
      */
     public Application(final CacheId cacheId) throws IOException, ParseException {
         this.cacheId = Assert.requireNonNull(cacheId, "cacheId");
-        this.size = cacheId.getFiles().stream().mapToLong(f -> f.getSize()).sum();
+        this.size = cacheId.getFiles().stream().mapToLong(CacheFile::getSize).sum();
         jnlpFile = new JNLPFile(Paths.get(cacheId.getId()).toUri().toURL());
     }
 
@@ -65,8 +65,8 @@ public class Application {
 
     public CompletableFuture<BufferedImage> loadIcon(final int dimension) {
         final CompletableFuture<BufferedImage> result = new CompletableFuture<>();
-        final URL iconURL = Optional.ofNullable(jnlpFile.getInformation().getIconLocation(IconKind.SHORTCUT, 64, 64))
-                .orElseGet(() -> jnlpFile.getInformation().getIconLocation(IconKind.DEFAULT, 64, 64));
+        final URL iconURL = Optional.ofNullable(jnlpFile.getInformation().getIconLocation(IconKind.SHORTCUT, dimension, dimension))
+                .orElseGet(() -> jnlpFile.getInformation().getIconLocation(IconKind.DEFAULT, dimension, dimension));
         if (iconURL == null) {
             Executors.newSingleThreadExecutor().submit(() -> {
                 try {
@@ -88,31 +88,6 @@ public class Application {
             });
         }
         return result;
-    }
-
-    /**
-     * Returns an icon that can be used to show the application
-     *
-     * @param dimension needed size of the icon
-     * @return the icon as image
-     */
-    public BufferedImage getIcon(final int dimension) {
-        // TODO We do not want to download the icon in the EDT. Maybe this method retunrs Future<BufferedImage>
-        // TODO jnlpFile.getInformation().getIcons()
-        return null;
-    }
-
-    /**
-     * Returns the URL of the JNLP file
-     *
-     * @return
-     */
-    public URL getJnlpFileUrl() {
-        try {
-            return Paths.get(cacheId.getId()).toUri().toURL();
-        } catch (final Exception e) {
-            throw new RuntimeException("Can not get JNLP URL", e);
-        }
     }
 
     public JNLPFile getJnlpFile() {
