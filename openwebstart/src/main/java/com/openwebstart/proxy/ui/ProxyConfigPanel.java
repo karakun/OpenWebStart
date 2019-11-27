f36d82aac248ebd82fe9cb7f14358019afdfe64a
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-11-27 19:22:20 +0100

    load / store config

--- openwebstart/src/main/java/com/openwebstart/proxy/ui/ProxyConfigPanel.java
+++ openwebstart/src/main/java/com/openwebstart/proxy/ui/ProxyConfigPanel.java
@@ -2,9 +2,13 @@ package com.openwebstart.proxy.ui;
 
 import com.openwebstart.controlpanel.FormPanel;
 import com.openwebstart.proxy.ProxyProviderTypes;
+import com.openwebstart.ui.Notifications;
 import net.adoptopenjdk.icedteaweb.Assert;
+import net.adoptopenjdk.icedteaweb.client.controlpanel.AdvancedProxySettingsDialog;
 import net.adoptopenjdk.icedteaweb.client.util.UiLock;
 import net.adoptopenjdk.icedteaweb.i18n.Translator;
+import net.adoptopenjdk.icedteaweb.logging.Logger;
+import net.adoptopenjdk.icedteaweb.logging.LoggerFactory;
 import net.sourceforge.jnlp.config.ConfigurationConstants;
 import net.sourceforge.jnlp.config.DeploymentConfiguration;
 
@@ -20,9 +24,13 @@ import javax.swing.SwingUtilities;
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class ProxyConfigPanel extends FormPanel {
 
+    private final static Logger LOG = LoggerFactory.getLogger(ProxyConfigPanel.class);
 
     private final JRadioButton noProxySelection;
 
@@ -34,6 +42,14 @@ public class ProxyConfigPanel extends FormPanel {
 
     private final JRadioButton useManualSettings;
 
+    private final JTextField pacUrlField;
+
+    private final JTextField proxyHostField;
+
+    private final JTextField proxyPortField;
+
+    private final JButton advancedButton;
+
     private final DeploymentConfiguration config;
 
     public ProxyConfigPanel(final DeploymentConfiguration config) {
@@ -44,31 +60,47 @@ public class ProxyConfigPanel extends FormPanel {
         final Translator translator = Translator.getInstance();
         final UiLock uiLock = new UiLock(config);
 
+        final FocusListener updateConfigOnFocusLost = new FocusAdapter() {
+            @Override
+            public void focusLost(final FocusEvent e) {
+                updateConfig();
+            }
+        };
+
         noProxySelection = new JRadioButton(translator.translate("proxyPanel.noProxy.text"));
+        noProxySelection.addActionListener(e -> onRadioButtonSelected(ProxyProviderTypes.NONE));
         noProxySelection.setToolTipText(translator.translate("proxyPanel.noProxy.description"));
+        uiLock.update(ConfigurationConstants.KEY_PROXY_TYPE, noProxySelection);
         basicConfigButtonGroup.add(noProxySelection);
         addRow(0, noProxySelection);
 
         useWinSystemSettings = new JRadioButton(translator.translate("proxyPanel.systemSettings.text"));
+        useWinSystemSettings.addActionListener(e -> onRadioButtonSelected(ProxyProviderTypes.WINDOWS));
         useWinSystemSettings.setToolTipText(translator.translate("proxyPanel.systemSettings.description"));
+        uiLock.update(ConfigurationConstants.KEY_PROXY_TYPE, useWinSystemSettings);
         basicConfigButtonGroup.add(useWinSystemSettings);
         if (ProxyProviderTypes.WINDOWS.isSupported()) {
             addRow(1, useWinSystemSettings);
         }
 
         useFirefoxSettings = new JRadioButton(translator.translate("proxyPanel.firefoxSettings.text"));
+        useFirefoxSettings.addActionListener(e -> onRadioButtonSelected(ProxyProviderTypes.FIREFOX));
         useFirefoxSettings.setToolTipText(translator.translate("proxyPanel.firefoxSettings.description"));
+        uiLock.update(ConfigurationConstants.KEY_PROXY_TYPE, useFirefoxSettings);
         basicConfigButtonGroup.add(useFirefoxSettings);
         if (ProxyProviderTypes.FIREFOX.isSupported()) {
             addRow(2, useFirefoxSettings);
         }
 
         usePacSettings = new JRadioButton(translator.translate("proxyPanel.pacSettings.text"));
+        usePacSettings.addActionListener(e -> onRadioButtonSelected(ProxyProviderTypes.MANUAL_PAC_URL));
         usePacSettings.setToolTipText(translator.translate("proxyPanel.pacSettings.description"));
+        uiLock.update(ConfigurationConstants.KEY_PROXY_TYPE, usePacSettings);
         basicConfigButtonGroup.add(usePacSettings);
         final FormPanel pacSettingsDetailsPanel = new FormPanel();
         final JLabel pacUrlLabel = new JLabel(translator.translate("proxyPanel.pacUrl.text") + ":");
-        final JTextField pacUrlField = new JTextField();
+        pacUrlField = new JTextField();
+        pacUrlField.addFocusListener(updateConfigOnFocusLost);
         pacUrlField.setToolTipText(translator.translate("proxyPanel.pacUrl.description"));
         pacSettingsDetailsPanel.addRow(0, pacUrlLabel, pacUrlField);
         pacSettingsDetailsPanel.setBorder(BorderFactory.createEmptyBorder(0, 64, 0, 12));
@@ -78,11 +110,13 @@ public class ProxyConfigPanel extends FormPanel {
         addRow(3, pacSettingsPanel);
 
         useManualSettings = new JRadioButton(translator.translate("proxyPanel.manualSettings.text"));
+        useManualSettings.addActionListener(e -> onRadioButtonSelected(ProxyProviderTypes.MANUAL_HOSTS));
         useManualSettings.setToolTipText(translator.translate("proxyPanel.manualSettings.description"));
+        uiLock.update(ConfigurationConstants.KEY_PROXY_TYPE, useManualSettings);
         basicConfigButtonGroup.add(useManualSettings);
         final JPanel manualSettingsDetailsPanel = new JPanel(new GridBagLayout());
         manualSettingsDetailsPanel.setBorder(BorderFactory.createEmptyBorder(0, 64, 0, 12));
-        final JLabel proxyHostLabel = new JLabel(translator.translate("proxyPanel.proxyHost.text") + ":");
+        final JLabel proxyHostLabel = new JLabel(translator.translate("proxyPanel.proxyAddress.text") + ":");
         final GridBagConstraints proxyHostLabelContraints = new GridBagConstraints();
         proxyHostLabelContraints.gridx = 0;
         proxyHostLabelContraints.gridy = 0;
@@ -90,8 +124,9 @@ public class ProxyConfigPanel extends FormPanel {
         proxyHostLabelContraints.weighty = 0;
         proxyHostLabelContraints.fill = GridBagConstraints.HORIZONTAL;
         manualSettingsDetailsPanel.add(proxyHostLabel, proxyHostLabelContraints);
-        final JTextField proxyHostField = new JTextField();
-        proxyHostField.setColumns(32);
+        proxyHostField = new JTextField();
+        proxyHostField.addFocusListener(updateConfigOnFocusLost);
+        proxyHostField.setToolTipText(translator.translate("proxyPanel.proxyHost.description"));
         proxyHostField.setToolTipText(translator.translate("proxyPanel.proxyHost.description"));
         final GridBagConstraints proxyHostFieldContraints = new GridBagConstraints();
         proxyHostFieldContraints.gridx = 1;
@@ -108,18 +143,23 @@ public class ProxyConfigPanel extends FormPanel {
         colonLabelContraints.weighty = 0;
         proxyHostFieldContraints.fill = GridBagConstraints.HORIZONTAL;
         manualSettingsDetailsPanel.add(colonLabel, colonLabelContraints);
-        final JTextField proxyPortField = new JTextField();
-        proxyPortField.setColumns(8);
+        proxyPortField = new JTextField();
+        proxyPortField.addFocusListener(updateConfigOnFocusLost);
+        proxyPortField.setColumns(4);
         proxyPortField.setToolTipText(translator.translate("proxyPanel.proxyPort.description"));
         final GridBagConstraints proxyPortFieldContraints = new GridBagConstraints();
         proxyPortFieldContraints.gridx = 3;
         proxyPortFieldContraints.gridy = 0;
-        proxyPortFieldContraints.weightx = 0.2;
+        proxyPortFieldContraints.weightx = 0;
         proxyPortFieldContraints.weighty = 0;
         proxyHostFieldContraints.fill = GridBagConstraints.HORIZONTAL;
         manualSettingsDetailsPanel.add(proxyPortField, proxyPortFieldContraints);
-        final JButton advancedButton = new JButton(translator.translate("proxyPanel.advanced.text"));
+        advancedButton = new JButton(translator.translate("proxyPanel.advanced.text"));
         advancedButton.setToolTipText(translator.translate("proxyPanel.advanced.description"));
+        advancedButton.addActionListener(e -> {
+            AdvancedProxySettingsDialog.showAdvancedProxySettingsDialog(config);
+            updateUi();
+        });
         final GridBagConstraints advancedButtonContraints = new GridBagConstraints();
         advancedButtonContraints.gridx = 4;
         advancedButtonContraints.gridy = 0;
@@ -133,30 +173,79 @@ public class ProxyConfigPanel extends FormPanel {
         addRow(4, manualSettingsPanel);
 
         addFlexibleRow(5);
+
+        updateUi();
+    }
+
+    private void onRadioButtonSelected(final ProxyProviderTypes providerType) {
+        updateEnabledStateForProxyType(providerType);
+        updateConfig();
+    }
+
+    private void updateEnabledStateForProxyType(final ProxyProviderTypes providerType) {
+        final boolean isPac = providerType == ProxyProviderTypes.MANUAL_PAC_URL;
+        pacUrlField.setEnabled(isPac);
+
+        final boolean isManual = providerType == ProxyProviderTypes.MANUAL_HOSTS;
+        proxyHostField.setEnabled(isManual);
+        proxyPortField.setEnabled(isManual);
+        advancedButton.setEnabled(isManual);
     }
 
     private void updateConfig() {
+        if (noProxySelection.isSelected()) {
+            config.setProperty(ConfigurationConstants.KEY_PROXY_TYPE, ProxyProviderTypes.NONE.getConfigValue() + "");
+        } else if (useFirefoxSettings.isSelected()) {
+            config.setProperty(ConfigurationConstants.KEY_PROXY_TYPE, ProxyProviderTypes.FIREFOX.getConfigValue() + "");
+        } else if (useWinSystemSettings.isSelected()) {
+            config.setProperty(ConfigurationConstants.KEY_PROXY_TYPE, ProxyProviderTypes.WINDOWS.getConfigValue() + "");
+        } else if (useManualSettings.isSelected()) {
+            config.setProperty(ConfigurationConstants.KEY_PROXY_TYPE, ProxyProviderTypes.MANUAL_HOSTS.getConfigValue() + "");
+        } else if (usePacSettings.isSelected()) {
+            config.setProperty(ConfigurationConstants.KEY_PROXY_TYPE, ProxyProviderTypes.MANUAL_PAC_URL.getConfigValue() + "");
+        }
 
+        config.setProperty(ConfigurationConstants.KEY_PROXY_AUTO_CONFIG_URL, pacUrlField.getText());
+        config.setProperty(ConfigurationConstants.KEY_PROXY_HTTP_HOST, proxyHostField.getText());
+        config.setProperty(ConfigurationConstants.KEY_PROXY_HTTP_PORT, proxyPortField.getText());
     }
 
     private void updateUi() {
-        final String proxyTypeString = config.getProperty(ConfigurationConstants.KEY_PROXY_TYPE);
-        final int proxyTypeConfigValue = Integer.valueOf(proxyTypeString);
-        final ProxyProviderTypes providerType = ProxyProviderTypes.getForConfigValue(proxyTypeConfigValue);
-        if (providerType == ProxyProviderTypes.NONE) {
+        try {
+            final String proxyTypeString = config.getProperty(ConfigurationConstants.KEY_PROXY_TYPE);
+            if (proxyTypeString == null) {
+                noProxySelection.setSelected(true);
+                updateEnabledStateForProxyType(ProxyProviderTypes.NONE);
+            } else {
+                final int proxyTypeConfigValue = Integer.valueOf(proxyTypeString);
+                final ProxyProviderTypes providerType = ProxyProviderTypes.getForConfigValue(proxyTypeConfigValue);
+                if (providerType == ProxyProviderTypes.NONE) {
+                    noProxySelection.setSelected(true);
+                } else if (providerType == ProxyProviderTypes.FIREFOX) {
+                    useFirefoxSettings.setSelected(true);
+                } else if (providerType == ProxyProviderTypes.WINDOWS) {
+                    useWinSystemSettings.setSelected(true);
+                } else if (providerType == ProxyProviderTypes.MANUAL_HOSTS) {
+                    useManualSettings.setSelected(true);
+                } else if (providerType == ProxyProviderTypes.MANUAL_PAC_URL) {
+                    usePacSettings.setSelected(true);
+                }
+                updateEnabledStateForProxyType(providerType);
+            }
+        } catch (final Exception e) {
+            Notifications.showError(Translator.getInstance().translate("proxyPanel.error.loadSettings"));
+            LOG.error("Error while loading proxy settings", e);
             noProxySelection.setSelected(true);
-        } else if (providerType == ProxyProviderTypes.FIREFOX) {
-            useFirefoxSettings.setSelected(true);
-        } else if (providerType == ProxyProviderTypes.WINDOWS) {
-            useWinSystemSettings.setSelected(true);
-        } else if (providerType == ProxyProviderTypes.MANUAL_HOSTS) {
-            useManualSettings.setSelected(true);
-        } else if (providerType == ProxyProviderTypes.MANUAL_PAC_URL) {
-            usePacSettings.setSelected(true);
+            updateEnabledStateForProxyType(ProxyProviderTypes.NONE);
         }
+        pacUrlField.setText(config.getProperty(ConfigurationConstants.KEY_PROXY_AUTO_CONFIG_URL));
+        proxyHostField.setText(config.getProperty(ConfigurationConstants.KEY_PROXY_HTTP_HOST));
+        proxyPortField.setText(config.getProperty(ConfigurationConstants.KEY_PROXY_HTTP_PORT));
     }
 
     public static void main(String[] args) throws Exception {
+        Translator.addBundle("i18n");
+
         SwingUtilities.invokeAndWait(() -> {
             JFrame frame = new JFrame();
             frame.setContentPane(new ProxyConfigPanel(new DeploymentConfiguration()));
