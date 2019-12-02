ab3d0eb7be64101e79bf84c0cf4273749a387cea
Stephan Classen <st.classen@gmx.ch>
2019-12-02 17:48:57 +0100

    update FirefoxProxyType to newest values from FF docu

--- openwebstart/src/main/java/com/openwebstart/proxy/firefox/FirefoxProxyType.java
+++ openwebstart/src/main/java/com/openwebstart/proxy/firefox/FirefoxProxyType.java
@@ -1,29 +1,41 @@
 package com.openwebstart.proxy.firefox;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
+/**
+ * See the following resources for the values assigned to the enums:
+ * http://kb.mozillazine.org/Network.proxy.type
+ * https://bugzilla.mozilla.org/show_bug.cgi?id=115720
+ * https://bugzilla.mozilla.org/show_bug.cgi?id=500983
+ * https://bugzilla.mozilla.org/attachment.cgi?id=392739&action=diff
+ */
 public enum FirefoxProxyType {
 
-    BROWSER_PROXY_TYPE_NONE(0),
+    BROWSER_PROXY_TYPE_NONE(0, 3),
     BROWSER_PROXY_TYPE_MANUAL(1),
     BROWSER_PROXY_TYPE_PAC(2),
     BROWSER_PROXY_TYPE_AUTO(4),
     BROWSER_PROXY_TYPE_SYSTEM(5);
 
     public int getConfigValue() {
-        return configValue;
+        return configValue.get(0);
     }
 
-    private final int configValue;
+    private final List<Integer> configValue = new ArrayList<>();
 
-    FirefoxProxyType(final int configValue) {
-        this.configValue = configValue;
+    FirefoxProxyType(final int main, final int... alt) {
+        configValue.add(main);
+        for (int i : alt) {
+            configValue.add(i);
+        }
     }
 
     public static FirefoxProxyType getForConfigValue(final int value) {
         return Stream.of(FirefoxProxyType.values())
-                .filter(t -> value == t.configValue)
+                .filter(t -> t.configValue.contains(value))
                 .findFirst()
-                .orElse(BROWSER_PROXY_TYPE_NONE);
+                .orElse(BROWSER_PROXY_TYPE_SYSTEM);
     }
 }
