09d5436b33bc1ddfbec0188ab4ea65e959ba03ea
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-12-05 12:05:33 +0100

    unused test removed

--- openwebstart/src/test/java/com/openwebstart/proxy/util/CidrUtilsTest.java
+++ openwebstart/src/test/java/com/openwebstart/proxy/util/CidrUtilsTest.java
@@ -19,23 +19,4 @@ class CidrUtilsTest {
         assertTrue(CidrUtils.isInRange(cidrNotation, lastInRange));
         assertFalse(CidrUtils.isInRange(cidrNotation, firstAfterRange));
     }
-
-    @Test
-    void testIsInRange2() {
-        //given:
-        final String cidrNotation = "169.254.1.80/30";
-
-        final String lastBeforeRange = "169.254.1.79";
-        final String firstInRange = "169.254.1.80";
-        final String someInRange = "169.254.1.81";
-        final String lastInRange = "169.254.1.83";
-        final String firstAfterRange = "169.254.1.84";
-
-        //than
-        assertFalse(CidrUtils.isInRange(cidrNotation, lastBeforeRange));
-        assertTrue(CidrUtils.isInRange(cidrNotation, firstInRange));
-        assertTrue(CidrUtils.isInRange(cidrNotation, someInRange));
-        assertTrue(CidrUtils.isInRange(cidrNotation, lastInRange));
-        assertFalse(CidrUtils.isInRange(cidrNotation, firstAfterRange));
-    }
 }
