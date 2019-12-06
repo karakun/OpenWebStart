a49aa7aa186ece3f1ab42a2ba5278b39732f67c9
Stephan Classen <st.classen@gmx.ch>
2019-12-06 17:13:15 +0100

    add tests

--- openwebstart/src/main/java/com/openwebstart/proxy/linux/GnomeProxyConfigReader.java
+++ openwebstart/src/main/java/com/openwebstart/proxy/linux/GnomeProxyConfigReader.java
@@ -5,6 +5,7 @@ import net.adoptopenjdk.icedteaweb.StringUtils;
 import net.adoptopenjdk.icedteaweb.logging.Logger;
 import net.adoptopenjdk.icedteaweb.logging.LoggerFactory;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -47,92 +48,103 @@ class GnomeProxyConfigReader {
 
         try {
             final String gnomeSettings = ProcessUtil.executeProcessAndReturnOutput("gsettings", "ist-recursively", "org.gnome.system.proxy");
-            final List<String> lines = splitIntoLines(gnomeSettings);
-
-            Map<String, String> values = lines.stream()
-                    .map(String::trim)
-                    .filter(line -> !line.isEmpty())
-                    .map(line -> line.split("\\s+", 3))
-                    .filter(parts -> parts.length == 3)
-                    .collect(Collectors.toMap(parts -> parts[0] + ":" + parts[1], parts -> parts[2]));
-
-            final LinuxProxySettings result = new LinuxProxySettings();
-
-
-            final String proxyMode = parseString(values.getOrDefault(GNOME_PROXY_MODE, MODE_NONE)).toLowerCase();
-            if (MODE_NONE.equals(proxyMode)) {
-                result.setMode(NO_PROXY);
-            } else if (MODE_AUTO.equals(proxyMode)) {
-                result.setMode(PAC);
-                result.setAutoConfigUrl(new URL(values.get(GNOME_PROXY_AUTOCONFIG_URL)));
-            } else if (MODE_MANUAL.equals(proxyMode)) {
-                result.setMode(MANUAL);
-                result.setAuthenticationEnabled(parseBoolean(values.get(GNOME_PROXY_USE_AUTHENTICATION)));
-                result.setExceptionsList(parseList(values.get(GNOME_PROXY_IGNORE_HOSTS)));
-                result.setLocalhostExcluded(containsLocalhost(result.getExceptionList()));
-
-                // socks is fallback for all other types
-                final String socksHost = parseString(values.get(GNOME_PROXY_SOCKS_HOST));
-                final int socksPort = parseInt(values.get(GNOME_PROXY_SOCKS_PORT));
-                if (!StringUtils.isBlank(socksHost) && socksPort > 0) {
-                    result.setSocksEnabled(true);
-                    result.setFtpEnabled(true);
-                    result.setHttpEnabled(true);
-                    result.setHttpsEnabled(true);
-
-                    result.setSocksHost(socksHost);
-                    result.setSocksPort(socksPort);
-
-                    result.setFtpHost(socksHost);
-                    result.setFtpPort(socksPort);
-
-                    result.setHttpHost(socksHost);
-                    result.setHttpPort(socksPort);
-
-                    result.setHttpsPort(socksPort);
-                    result.setHttpsHost(socksHost);
-                }
-
-                // ftp overwrites socks
-                final String ftpHost = parseString(values.get(GNOME_PROXY_FTP_HOST));
-                final int ftpPort = parseInt(values.get(GNOME_PROXY_FTP_PORT));
-                if (!StringUtils.isBlank(ftpHost) && ftpPort > 0) {
-                    result.setFtpEnabled(true);
-                    result.setFtpPort(ftpPort);
-                    result.setFtpHost(ftpHost);
-                }
-
-                // http overwrites socks and is fallback for https
-                final String httpHost = parseString(values.get(GNOME_PROXY_HTTP_HOST));
-                final int httpPort = parseInt(values.get(GNOME_PROXY_HTTP_PORT));
-                if (!StringUtils.isBlank(httpHost) && httpPort > 0) {
-                    result.setHttpEnabled(true);
-                    result.setHttpsEnabled(true);
-
-                    result.setHttpHost(httpHost);
-                    result.setHttpPort(httpPort);
-
-                    result.setHttpsHost(httpHost);
-                    result.setHttpsPort(httpPort);
-                }
-
-                // https overwrites both socks and http
-                final String httpsHost = parseString(values.get(GNOME_PROXY_HTTPS_HOST));
-                final int httpsPort = parseInt(values.get(GNOME_PROXY_HTTPS_PORT));
-                if (!StringUtils.isBlank(httpsHost) && httpsPort > 0) {
-                    result.setHttpsEnabled(true);
-                    result.setHttpsPort(httpsPort);
-                    result.setHttpsHost(httpsHost);
-                }
-
-            } else {
-                throw new RuntimeException("found unknown gnome proxy mode: " + proxyMode);
-            }
+            final Map<String, String> values = parseGnomeSettings(gnomeSettings);
+            final LinuxProxySettings result = convertToSettings(values);
             return Optional.of(result);
         } catch (Exception e) {
             LOG.debug("Could not load gnome proxy settings {}", e.getMessage());
             return Optional.empty();
         }
+    }
+
+
+    /** visible for testing */
+    static Map<String, String> parseGnomeSettings(String gnomeSettings) {
+        final List<String> lines = splitIntoLines(gnomeSettings);
+
+        return lines.stream()
+                .map(String::trim)
+                .filter(line -> !line.isEmpty())
+                .map(line -> line.split("\\s+", 3))
+                .filter(parts -> parts.length == 3)
+                .collect(Collectors.toMap(parts -> parts[0] + ":" + parts[1], parts -> parts[2]));
+    }
+
+    /** visible for testing */
+    static LinuxProxySettings convertToSettings(Map<String, String> values) throws MalformedURLException {
+        final LinuxProxySettings result = new LinuxProxySettings();
+
+
+        final String proxyMode = parseString(values.getOrDefault(GNOME_PROXY_MODE, MODE_NONE)).toLowerCase();
+        if (MODE_NONE.equals(proxyMode)) {
+            result.setMode(NO_PROXY);
+        } else if (MODE_AUTO.equals(proxyMode)) {
+            result.setMode(PAC);
+            result.setAutoConfigUrl(new URL(parseString(values.get(GNOME_PROXY_AUTOCONFIG_URL))));
+        } else if (MODE_MANUAL.equals(proxyMode)) {
+            result.setMode(MANUAL);
+            result.setAuthenticationEnabled(parseBoolean(values.get(GNOME_PROXY_USE_AUTHENTICATION)));
+            result.setExceptionsList(parseList(values.get(GNOME_PROXY_IGNORE_HOSTS)));
+            result.setLocalhostExcluded(containsLocalhost(result.getExceptionList()));
+
+            // socks is fallback for all other types
+            final String socksHost = parseString(values.get(GNOME_PROXY_SOCKS_HOST));
+            final int socksPort = parseInt(values.get(GNOME_PROXY_SOCKS_PORT));
+            if (!StringUtils.isBlank(socksHost) && socksPort > 0) {
+                result.setSocksEnabled(true);
+                result.setFtpEnabled(true);
+                result.setHttpEnabled(true);
+                result.setHttpsEnabled(true);
+
+                result.setSocksHost(socksHost);
+                result.setSocksPort(socksPort);
+
+                result.setFtpHost(socksHost);
+                result.setFtpPort(socksPort);
+
+                result.setHttpHost(socksHost);
+                result.setHttpPort(socksPort);
+
+                result.setHttpsPort(socksPort);
+                result.setHttpsHost(socksHost);
+            }
+
+            // ftp overwrites socks
+            final String ftpHost = parseString(values.get(GNOME_PROXY_FTP_HOST));
+            final int ftpPort = parseInt(values.get(GNOME_PROXY_FTP_PORT));
+            if (!StringUtils.isBlank(ftpHost) && ftpPort > 0) {
+                result.setFtpEnabled(true);
+                result.setFtpPort(ftpPort);
+                result.setFtpHost(ftpHost);
+            }
+
+            // http overwrites socks and is fallback for https
+            final String httpHost = parseString(values.get(GNOME_PROXY_HTTP_HOST));
+            final int httpPort = parseInt(values.get(GNOME_PROXY_HTTP_PORT));
+            if (!StringUtils.isBlank(httpHost) && httpPort > 0) {
+                result.setHttpEnabled(true);
+                result.setHttpsEnabled(true);
+
+                result.setHttpHost(httpHost);
+                result.setHttpPort(httpPort);
+
+                result.setHttpsHost(httpHost);
+                result.setHttpsPort(httpPort);
+            }
+
+            // https overwrites both socks and http
+            final String httpsHost = parseString(values.get(GNOME_PROXY_HTTPS_HOST));
+            final int httpsPort = parseInt(values.get(GNOME_PROXY_HTTPS_PORT));
+            if (!StringUtils.isBlank(httpsHost) && httpsPort > 0) {
+                result.setHttpsEnabled(true);
+                result.setHttpsPort(httpsPort);
+                result.setHttpsHost(httpsHost);
+            }
+
+        } else {
+            throw new RuntimeException("found unknown gnome proxy mode: " + proxyMode);
+        }
+        return result;
     }
 
     private static boolean containsLocalhost(List<String> exceptionList) {
