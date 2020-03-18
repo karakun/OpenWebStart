c734796ce75d323870b907160d44181dd27f27f4
AndreasEhret <andreas.ehret@karakun.com>
2020-03-18 11:40:23 +0100

    added another name variant to AzulResolver

--- openwebstart/src/main/java/com/openwebstart/jvm/vendor/AzulResolver.java
+++ openwebstart/src/main/java/com/openwebstart/jvm/vendor/AzulResolver.java
@@ -5,6 +5,6 @@ import static com.openwebstart.jvm.runtimes.Vendor.AZUL;
 public class AzulResolver extends BasicVendorResolver {
 
     public AzulResolver() {
-        super(AZUL, "Azul", "Zulu", "Azul Zulu", "Azul Systems", "Azul Systems Inc.");
+        super(AZUL, "Azul", "Zulu", "Azul Zulu", "Azul Systems", "Azul Systems Inc.", "Zulu Community Edition");
     }
 }
