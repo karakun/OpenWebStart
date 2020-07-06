242b8eb87d2bb119ba5ab6ca2150182603781233
NashkevichV <vn@vin-soft.org>
2020-07-06 14:15:54 +0200

    ScriptFactory : start script for JNLP-applicaion under macos.
    Start scripts tries to open jnlp-url without explicit reference to
    openwebstart.

--- openwebstart/src/main/java/com/openwebstart/os/ScriptFactory.java
+++ openwebstart/src/main/java/com/openwebstart/os/ScriptFactory.java
@@ -1,25 +1,23 @@
 package com.openwebstart.os;
 
-import com.openwebstart.install4j.Install4JUtils;
-import com.openwebstart.jvm.os.OperationSystem;
-import net.adoptopenjdk.icedteaweb.logging.Logger;
-import net.adoptopenjdk.icedteaweb.logging.LoggerFactory;
-import net.sourceforge.jnlp.JNLPFile;
-
 import java.io.IOException;
+import java.net.URL;
 import java.util.Optional;
 
-public class ScriptFactory {
+import com.openwebstart.install4j.Install4JUtils;
+import com.openwebstart.jvm.os.OperationSystem;
 
-    private final static Logger LOG = LoggerFactory.getLogger(ScriptFactory.class);
+import net.sourceforge.jnlp.JNLPFile;
 
+public class ScriptFactory 
+{
     private final static String SCRIPT_START = "#!/bin/sh";
 
     private static final String JAVA_WS_NAME = "javaws";
 
     public static String createStartScript(final JNLPFile jnlpFile) {
         if(OperationSystem.getLocalSystem() == OperationSystem.MAC64) {
-            return createStartScriptForMac(jnlpFile);
+            return createSimpleStartScriptForMac(jnlpFile);
         }
         final String executable = Install4JUtils.installationDirectory()
                 .map(d -> d + "/" + JAVA_WS_NAME)
@@ -39,6 +37,22 @@ public class ScriptFactory {
         final String jnlpLocation = "\"" + jnlpFile.getFileLocation() + "\"";
 
         return SCRIPT_START + System.lineSeparator() + "open -a " + executable + " " + jnlpLocation;
+    }
+
+    public static String createSimpleStartScriptForMac(final JNLPFile jnlpFile) 
+    {
+    	final URL 		srcUrl = jnlpFile.getSourceLocation();
+    	final String	schema = srcUrl.getProtocol();
+    	final String	url;
+   		if ( "http".equalsIgnoreCase(schema) || "https".equalsIgnoreCase(schema) )
+   		{
+   			url = "jnlp" + srcUrl.toString().substring( 4 );
+    	}
+   		else
+   		{
+   			url = srcUrl.toString();
+   		}
+        return SCRIPT_START + System.lineSeparator() + "open \"" + url + "\"";
     }
 
     public static Process createStartProcess(final JNLPFile jnlpFile) throws IOException {
