bef00e1edfc298b0c4a88b237407219a6f0bde81
Stephan Classen <st.classen@gmx.ch>
2021-05-06 12:40:17 +0200

    add more vendor names for amazon

--- openwebstart/src/main/java/com/openwebstart/jvm/vendor/AmazonResolver.java
+++ openwebstart/src/main/java/com/openwebstart/jvm/vendor/AmazonResolver.java
@@ -5,7 +5,7 @@ import static com.openwebstart.jvm.runtimes.Vendor.AMAZON;
 public class AmazonResolver extends BasicVendorResolver {
 
     public AmazonResolver() {
-        super(AMAZON, "amazon", "Amazon Inc.");
+        super(AMAZON, "amazon", "Amazon Inc.", "Amazon.com Inc.");
     }
 
 }
