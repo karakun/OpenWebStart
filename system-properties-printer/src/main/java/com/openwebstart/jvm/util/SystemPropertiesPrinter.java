97aa3aa9c84303bd0fc0c2786c122a061b4ab804
Csipak Attila <acsipak@gmail.com>
2021-12-03 17:54:34 +0100

    GitHub issue #464 If we only want to print the properties required by JavaRuntimePropertiesDetector, things get a lot simpler.

--- system-properties-printer/src/main/java/com/openwebstart/jvm/util/SystemPropertiesPrinter.java
+++ system-properties-printer/src/main/java/com/openwebstart/jvm/util/SystemPropertiesPrinter.java
@@ -2,12 +2,18 @@ package com.openwebstart.jvm.util;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableSet;
 
 /**
- * Emulate the -XshowSettings:properties nonstandard option on non-Oracle and pre-1.8 JVM versions.
- * This class should be compiled using pre-1.8 source and target settings.
+ * Emulates the -XshowSettings:properties nonstandard option on non-Oracle and pre-1.8 JVM versions. The main
+ * difference is that we only print a small subset of the system properties required by OWS
+ * JavaRuntimePropertiesDetector. This class should be compiled using pre-1.8 source and target settings.
  * Currently, it is written to conform the 1.6 language level.
  * @see <a href="https://docs.oracle.com/javase/8/docs/technotes/tools/windows/java.html#BABHDABI">
  *     Oracle Java SE 8 Tools Reference java Nonstandard Options</a>
@@ -15,77 +21,27 @@ import java.util.Properties;
 @SuppressWarnings("UseOfSystemOutOrSystemErr")
 public final class SystemPropertiesPrinter {
 
-    private static final int CR_BYTE_VALUE = 10;
-    private static final int LF_BYTE_VALUE = 13;
+    /**
+     * Must match the contents of JavaRuntimePropertiesDetector.REQUIRED_PROPS
+     */
+    @SuppressWarnings("StaticCollection")
+    private static final Set<String> REQUIRED_PROPS =
+            unmodifiableSet(new HashSet<String>(asList("java.vendor", "java.version", "os.name", "os.arch")));
 
     private SystemPropertiesPrinter() {
         // Utility class, do not instantiate.
     }
 
-    /**
-     * <p>Print the system properties to standard error in a similar way as the following Oracle Java 1.8 command would:</p>
-     * {@code java -XshowSettings -version}
-     */
     public static void main(String[] args) {
         Properties systemProperties = System.getProperties();
         System.err.println("Property settings:");
         List<String> propertyNames = new ArrayList<String>(systemProperties.stringPropertyNames());
         Collections.sort(propertyNames);
         for (String propertyName : propertyNames) {
-            printPropertyValue(propertyName, systemProperties.getProperty(propertyName));
+            if (REQUIRED_PROPS.contains(propertyName)) {
+                System.err.print("    " + propertyName + " = " + systemProperties.getProperty(propertyName));
+            }
         }
         System.err.println();
-    }
-
-    /**
-     * Print the property key and value supplied in the parameters using a simple formatting:
-     * <ul>
-     *     <li>
-     *         The line.separator property may contain nonprintable characters (CR and LF),
-     *         which are replaced with their respective escape strings (\n and \r).
-     *     </li>
-     *     <li>If a property contains multiple paths, they are printed on separate lines for readability.</li>
-     * </ul>
-     */
-    private static void printPropertyValue(String propertyName, String propertyValue) {
-        System.err.print("    " + propertyName + " = ");
-        if ("line.separator".equals(propertyName)) {
-            byte[] propertyValueBytes = propertyValue.getBytes();
-            for (byte propertyValueByte : propertyValueBytes) {
-                switch (propertyValueByte) {
-                    case CR_BYTE_VALUE:
-                        System.err.print("\\n ");
-                        break;
-                    case LF_BYTE_VALUE:
-                        System.err.print("\\r ");
-                        break;
-                    default:
-                        //noinspection MagicNumber
-                        System.err.printf("0x%02X", Integer.valueOf(propertyValueByte & 255));
-                }
-            }
-            System.err.println();
-        } else if (!isPath(propertyName)) {
-            System.err.println(propertyValue);
-        } else {
-            String[] paths = propertyValue.split(System.getProperty("path.separator"));
-            boolean firstPath = true;
-            for (String path : paths) {
-                if (firstPath) {
-                    System.err.println(path);
-                    firstPath = false;
-                } else {
-                    System.err.println("        " + path);
-                }
-            }
-        }
-    }
-
-    /**
-     * @param propertyName System property key.
-     * @return Does this property contain (zero or more) paths?
-     */
-    private static boolean isPath(String propertyName) {
-        return propertyName.endsWith(".dirs") || propertyName.endsWith(".path");
     }
 }
