4f5d59b550335270d0aede338166fd4f5e80eebf
janak <janak.mulani@karakun.com>
2022-02-11 11:45:57 +0530

    Add comments

--- openwebstart/src/main/java/com/openwebstart/os/ShortcutUpdateStrategy.java
+++ openwebstart/src/main/java/com/openwebstart/os/ShortcutUpdateStrategy.java
@@ -8,17 +8,17 @@ public enum ShortcutUpdateStrategy implements Translatable {
     /**
      * Overwrite the existing shortcuts.
      */
-    OVERWRITE ("desktop.integration.updateStrategy.overwrite"),
+    OVERWRITE ("desktop.integration.shortcutUpdateStrategy.overwrite"),
 
     /**
      * Keep the existing shortcuts as they are.
      */
-    KEEP ("desktop.integration.updateStrategy.keep"),
+    KEEP ("desktop.integration.shortcutUpdateStrategy.keep"),
 
     /**
      * Create the shortcut with unique name
      */
-    UNIQUE_NAME ("desktop.integration.updateStrategy.unique");
+    UNIQUE_NAME ("desktop.integration.shortcutUpdateStrategy.uniqueName");
 
     private final String translationKey;
 
@@ -32,11 +32,11 @@ public enum ShortcutUpdateStrategy implements Translatable {
     }
 
     public static ShortcutUpdateStrategy get(final String propertyValue) {
-        for(ShortcutUpdateStrategy strategy : ShortcutUpdateStrategy.values()) {
+        for(final ShortcutUpdateStrategy strategy : ShortcutUpdateStrategy.values()) {
             if (strategy.name().equals(propertyValue)) {
                 return strategy;
             }
         }
-        return OwsDefaultsProvider.SHORTCUT_UPDATE_STRATEGY_DEFAULT_VALUE;
+        return OwsDefaultsProvider.DEFAULT_SHORTCUT_UPDATE_STRATEGY;
     }
 }
