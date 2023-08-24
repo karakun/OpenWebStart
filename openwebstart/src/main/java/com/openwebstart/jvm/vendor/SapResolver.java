c7196c575150a98374d018eeb1a6a4193e9d96b3
mitja <25105259+nice2mitja@users.noreply.github.com>
2023-08-24 08:41:57 +0200

    Create SapResolver.java

---
+++ openwebstart/src/main/java/com/openwebstart/jvm/vendor/SapResolver.java
@@ -0,0 +1,11 @@
+package com.openwebstart.jvm.vendor;
+
+import static com.openwebstart.jvm.runtimes.Vendor.SAP;
+
+public class SapResolver extends BasicVendorResolver {
+
+    public SapResolver() {
+        super(SAP, "sap", "SAP SE", "SAP AG");
+    }
+
+}
